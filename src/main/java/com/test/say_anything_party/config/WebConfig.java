824418a47437caf1a9939e510114f7fb76627bf3
tracer12 <vendetta12@naver.com>
2025-03-28 18:54:12 +0900

    fix: 백엔드 완성

--- src/main/java/com/test/say_anything_party/config/WebConfig.java
+++ src/main/java/com/test/say_anything_party/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해 CORS 설정을 추가
         registry.addMapping("/**") // 모든 URL 경로에 대해 CORS 허용
-                .allowedOrigins("http://127.0.0.1:5503") // 로컬 클라이언트 주소
+                .allowedOrigins("http://127.0.0.1:5503", "http://localhost:3000") // 로컬 클라이언트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보를 허용
